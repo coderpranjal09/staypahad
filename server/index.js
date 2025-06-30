1e3796cb5254a490e8652db3b5fe07a85b4877e1
Pranjal Maithani <pranjalmaithani81822@gmail.com>
2025-06-30 19:03:48 +0530

    Initial commit from VS Code project

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(cors({
 }));
 app.use(cookieParser()); 
 // Routes
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
   res.json({ message: "Backend is working!" });
 });
 app.use('/api/homestays', homestayRoutes);
